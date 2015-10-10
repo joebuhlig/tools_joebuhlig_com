55eeb853fad9bef470ae22d9e9f80379149ebe48
Joe Buhlig <joe@joebuhlig.com>
2015-10-10 14:05:13 -0500

    Switched to postgres

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'tools.joebuhlig.com'
+set :repo_url, 'git@github.com:joebuhlig/tools_joebuhlig_com.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
