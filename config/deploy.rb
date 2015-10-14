6ccf32f185399c70f0c685f83e538fe5af53d84d
Joe Buhlig <joe@joebuhlig.com>
2015-10-14 08:55:54 -0500

    Wordsmithing and CSS/JS updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,13 +36,13 @@ set :repo_url, 'git@github.com:joebuhlig/tools_joebuhlig_com.git'
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
+			# Here we can do anything such as:
+			# within release_path do
+			#   execute :rake, 'cache:clear'
+			# end
+		end
+	end
 
 end
