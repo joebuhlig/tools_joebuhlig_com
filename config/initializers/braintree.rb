56a8a636dd7fa57fb13c0fd9b0c6f1c6a0122c24
Joe Buhlig <joe@joebuhlig.com>
2015-11-04 06:01:45 -0600

    Fixed braintree and added Reddit

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
-Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.environment = :production
 Braintree::Configuration.merchant_id = Rails.application.secrets.BRAINTREE_MERCHANT_ID
 Braintree::Configuration.public_key = Rails.application.secrets.BRAINTREE_PUBLIC_KEY
 Braintree::Configuration.private_key = Rails.application.secrets.BRAINTREE_PRIVATE_KEY
