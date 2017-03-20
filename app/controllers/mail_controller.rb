1440777fad94044cc22d16a483cebcd26e706acc
Joe Buhlig <joe@joebuhlig.com>
2017-03-20 07:52:33 -0500

    Fix for https on joebuhlig.com

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -3,7 +3,7 @@ class MailController < ApplicationController
 
 	def index
 		redirectLoc = "https://en.wikipedia.org/wiki/Main_Page"
-		if (request.headers['origin'] == 'http://joebuhlig.com') && (params["_gotcha"] == '')
+		if (request.headers['origin'] == 'https://joebuhlig.com') && (params["_gotcha"] == '')
 			MailMailer.main(params).deliver_now
 			if params["_next"]
 				redirectLoc = params["_next"]
